55760df185ece47f8e925006bca06c99e2fc746c
bcomnes <bcomnes@gmail.com>
2015-12-22 10:52:31 -0800

    fixed pbcopy stuff

--- osx_arch_alias.sh
+++ osx_arch_alias.sh
@@ -1,5 +1,8 @@
 # osx to arch alias'
 alias open='gnome-open'
 alias subl='subl3'
-alias pbcopy='xsel --clipboard --input'
-alias pbpaste='xsel --clipboard --output'
+alias pbcopy='xclip -selection clipboard'
+alias pbpaste='xclip -selection clipboard -o'
+#alias pbcopy='xsel --clipboard --input'
+#alias pbpaste='xsel --clipboard --output'
+
