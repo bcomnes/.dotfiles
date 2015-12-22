902a77d0964e032a47389474ba8879d5acaca196
bcomnes <bcomnes@gmail.com>
2015-12-22 09:09:20 -0800

    Added gnome keyring info link

--- gnome_keyring.sh
+++ gnome_keyring.sh
@@ -1,4 +1,5 @@
 # enable gnome keyring
+# https://wiki.archlinux.org/index.php/GNOME_Keyring#With_a_display_manager
 if [ -n "$DESKTOP_SESSION" ];then
     eval $(gnome-keyring-daemon --start)
     export SSH_AUTH_SOCK
