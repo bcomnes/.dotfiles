c477318e88b2d4fb876d9569494299141a935699
Bret Comnes <bcomnes@gmail.com>
2016-05-04 22:15:47 -0700

    Updated bork stuff

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -1,6 +1,12 @@
 cd ~
-ok github bcomnes/.dotfiles
+#ok github bcomnes/.dotfiles
 
 for config in $HOME/.dotfiles/configs/*; do
+  ok symlink ".$(basename $config)" $config
+done
+
+cd ~/.ssh
+
+for config in $HOME/.dotfiles/ssh/*; do
   ok symlink ".$(basename $config)" $config
 done
