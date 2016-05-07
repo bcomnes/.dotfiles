ca6fbbb34888ed5d19cc64ee813b032721aef9e8
Bret Comnes <bcomnes@gmail.com>
2016-05-06 22:02:09 -0700

    Improve bootstrapping of dotfiles by resolving ssh known_hosts stages

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -1,11 +1,15 @@
 cd ~
-#ok github bcomnes/.dotfiles
-
+ok github bcomnes/.dotfiles
 for config in $HOME/.dotfiles/configs/*; do
   ok symlink ".$(basename $config)" $config
 done
 
 cd ~/.ssh
+ok check [ -L ./known_hosts ]
+if check_failed; then
+  echo "moving ~/.ssh/known_hosts to ./known_hosts.bk"
+  mv ./known_hosts ./known_hosts.bk
+fi
 
 for config in $HOME/.dotfiles/ssh/*; do
   ok symlink "$(basename $config)" $config
