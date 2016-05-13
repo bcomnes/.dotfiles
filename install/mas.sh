37169137ea2fc045f6323b35c053566a6aa1b895
Bret Comnes <bcomnes@gmail.com>
2016-05-13 09:19:15 -0700

    disable freckle, enable xcode

--- install/mas.sh
+++ install/mas.sh
@@ -2,8 +2,8 @@ ok mas 639944780 LightBlue
 ok mas 411369012 "Contacts Cleaner"
 ok mas 411372497 "Calendar Cleaner"
 ok mas 715768417 "Microsoft Remote Desktop"
-ok mas 879917538 Freckle
+# ok mas 879917538 Freckle
 ok mas 692867256 Simplenote
 ok mas 566042322 "123D Design"
 ok mas 924726344 Deliveries
-# ok mas 497799835 Xcode
+ok mas 497799835 Xcode
