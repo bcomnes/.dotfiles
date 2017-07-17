aac5eb8ad0b66a22cf2f328b750ae5b12fa7f20f
Bret Comnes <bcomnes@gmail.com>
2017-07-16 19:15:26 -0700

    Checkign in work

--- configs/bashrc.d/darwin/bash_completion2.sh
+++ configs/bashrc.d/darwin/bash_completion2.sh
@@ -1,4 +1,4 @@
 # bash completion 2
-if [ -f $(brew --prefix)/share/bash-completion/bash_completion ];
-  then . $(brew --prefix)/share/bash-completion/bash_completion ;
+if [ -f $(brew --prefix)/share/bash-completion/bash_completion ]; then
+  . $(brew --prefix)/share/bash-completion/bash_completion ;
 fi
