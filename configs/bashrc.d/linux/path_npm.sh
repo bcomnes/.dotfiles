57bb36facd6560ef606b77194766357d66b1ce57
Bret Comnes <bcomnes@gmail.com>
2019-07-20 18:46:18 +0200

    Fix npm thing

--- configs/bashrc.d/linux/path_npm.sh
+++ configs/bashrc.d/linux/path_npm.sh
@@ -1,3 +1,5 @@
 # npm ~ global path
 # https://docs.npmjs.com/getting-started/fixing-npm-permissions
-export PATH="$(npm config get prefix)/bin:$PATH"
+if [ -x "$(command -v docker)" ]; then
+  export PATH="$(npm config get prefix)/bin:$PATH";
+fi
