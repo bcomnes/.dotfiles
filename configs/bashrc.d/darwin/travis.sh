d942d72151d479b419347d51f4b79162983a83ab
Bret Comnes <bcomnes@gmail.com>
2019-07-20 18:32:51 +0200

    More stuff

---
+++ configs/bashrc.d/darwin/travis.sh
@@ -0,0 +1,2 @@
+# added by travis gem
+[ -f ~/.travis/travis.sh ] && source ~/.travis/travis.sh
--- configs/bashrc.d/ps1.sh
+++ configs/bashrc.d/ps1.sh
@@ -1,4 +1,7 @@
 # ps1
 # https://wiki.archlinux.org/index.php/Color_Bash_Prompt#Professional_prompts
 #PS1='\[\e[0;32m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;32m\]\$\[\e[m\] '
-export PS1="\[\e[33;1;32m\]\h:\[\e[33;1;34m\]\W \u\[\e[1;32m\]\$\[\e[0m\] "
+# my old one
+# export PS1="\[\e[33;1;32m\]\h:\[\e[33;1;34m\]\W \u\[\e[1;32m\]\$\[\e[0m\] "
+# mafintoshes
+export PS1='\[\e[37;44m\] \W \[\e[m\]\[\e[37;46m\]$(__git_ps1 " %s ")\[\e[m\] '
