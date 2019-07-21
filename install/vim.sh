bc753a444981f6f410043ff6e035312d7ba3efda
Bret Comnes <bcomnes@gmail.com>
2019-07-21 18:24:52 +0200

    Conditional vim stuff

--- install/vim.sh
+++ install/vim.sh
@@ -2,7 +2,14 @@ echo "Setting up Vim"
 
 mkdir -p ~/.vim{backup,tmp,undo}
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+else
+  pushd ~/.vim/bundle/Vundle.vim
+  git pull
+  popd
+fi
+
 vim +PluginInstall +qall
 
 echo "Done setting up vim"
