e13d147ca5cd27e0cf8d2d13c44cb397cfaf1870
Bret Comnes <bcomnes@gmail.com>
2019-07-27 16:52:39 +0200

    improve npm pathing

--- configs/bashrc.d/linux/path_npm.sh
+++ configs/bashrc.d/linux/path_npm.sh
@@ -1,5 +1,6 @@
 # npm ~ global path
 # https://docs.npmjs.com/getting-started/fixing-npm-permissions
-if [ -x "$(command -v docker)" ]; then
+if [ -x "$(command -v npm)" ]; then
+	export NPM_CONFIG_PREFIX=~/.npm-global
   export PATH="$(npm config get prefix)/bin:$PATH";
 fi
