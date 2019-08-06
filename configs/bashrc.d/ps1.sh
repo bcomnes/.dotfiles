11f902c76be192306e706d081d18b5e80ced8a2d
Bret Comnes <bcomnes@gmail.com>
2019-08-06 11:51:16 +0200

    Fix some ps1 stuff

--- configs/bashrc.d/ps1.sh
+++ configs/bashrc.d/ps1.sh
@@ -4,10 +4,12 @@
 # my old one
 # export PS1="\[\e[33;1;32m\]\h:\[\e[33;1;34m\]\W \u\[\e[1;32m\]\$\[\e[0m\] "
 # mafintoshes
-export PS1='\[\e[37;44m\] \W \[\e[m\]\[\e[37;46m\]$(__git_ps1 " %s ")\[\e[m\] '
+# export PS1='\[\e[37;44m\] \W \[\e[m\]\[\e[37;46m\]$(__git_ps1 " %s ")\[\e[m\] '
 
-if [ "$(command -v __git_ps1)" ]; then
-  export PS1='\[\e[37;44m\] \W \[\e[m\]\[\e[37;46m\]$(__git_ps1 " %s ")\[\e[m\] '
-else
-  export PS1="\[\e[33;1;32m\]\h:\[\e[33;1;34m\]\W \u\[\e[1;32m\]\$\[\e[0m\] "
+if [ -n "$BASH_VERSION" ]; then
+  if [ "$(command -v __git_ps1)" ]; then
+    export PS1='\[\e[37;44m\] \W \[\e[m\]\[\e[37;46m\]$(__git_ps1 " %s ")\[\e[m\] '
+  else
+    export PS1="\[\e[33;1;32m\]\h:\[\e[33;1;34m\]\W \u\[\e[1;32m\]\$\[\e[0m\] "
+  fi
 fi
