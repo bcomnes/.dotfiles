ea8047c47fa8948ca6c0d99ffed30af4e5ef89a2
Bret Comnes <bcomnes@gmail.com>
2020-01-21 09:20:18 -0700

    Updates

--- configs/bashrc.d/darwin/zsh-completion.sh
+++ configs/bashrc.d/darwin/zsh-completion.sh
@@ -1,8 +1,8 @@
 # zsh completion
 if [ -d "$(brew --prefix)/share/zsh-completions" ] && [[ -n "$ZSH_VERSION" ]]; then
   HOMEBREW_COMPLETIONS=$(brew --prefix)/share/zsh-completions
-  CURRENT_COMPLETION="$( echo $fpath )"
+  CURRENT_COMPLETION="$( echo $FPATH )"
   if [[ ! $CURRENT_COMPLETION == *"${HOMEBREW_COMPLETIONS}"* ]]; then
-    fpath=($HOMEBREW_COMPLETIONS $fpath)
+    FPATH="$HOMEBREW_COMPLETIONS:$FPATH"
   fi
 fi
--- configs/bashrc.d/zsh.sh
+++ configs/bashrc.d/zsh.sh
@@ -1,5 +1,4 @@
 # https://scriptingosx.com/2019/07/moving-to-zsh-part-5-completions/
-
 # zsh completion
 if [[ -n "$ZSH_VERSION" ]]; then
   autoload -Uz compinit
