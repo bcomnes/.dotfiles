6fa66a770f92c8950d492e3d6a3cf0106e64e469
Bret Comnes <bcomnes@gmail.com>
2023-02-08 17:22:14 -0700

    Delete bash completions

--- configs/bashrc.d/darwin/bash_completion2.sh
+++
@@ -1,5 +0,0 @@
-# bash completion 2
-if [ -f "$(brew --prefix)/etc/profile.d/bash_completion.sh" ] && [ "${BASH_VERSINFO:-0}" -ge 4 ]; then
-  export BASH_COMPLETION_COMPAT_DIR="$(brew --prefix)/etc/bash_completion.d"
-  . $(brew --prefix)/etc/profile.d/bash_completion.sh ;
-fi
