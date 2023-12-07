411380ff48d09a44977d0906398eee5bf94a078a
Bret Comnes <bcomnes@gmail.com>
2023-12-06 16:21:44 -0800

    Fix zsh completions

--- configs/bashrc.d/darwin/youtube-dl.sh
+++ configs/bashrc.d/darwin/youtube-dl.sh
@@ -1,4 +1,4 @@
 # use yt-dlp instead of youtube-dl
 if [ -x "$(command -v yt-dlp)" ]; then
-  alias youtube-dl="yt-dlp --cookies-from-browser firefox";
+  alias youtube-dl="yt-dlp --cookies-from-browser firefox --trim-filenames 100";
 fi
--- configs/bashrc.d/darwin/zsh-completion.sh
+++ configs/bashrc.d/darwin/zsh-completion.sh
@@ -3,6 +3,8 @@ if [ -d "$(brew --prefix)/share/zsh-completions" ] && [[ -n "$ZSH_VERSION" ]]; t
   HOMEBREW_COMPLETIONS=$(brew --prefix)/share/zsh-completions
   CURRENT_COMPLETION="$( echo $FPATH )"
   if [[ ! $CURRENT_COMPLETION == *"${HOMEBREW_COMPLETIONS}"* ]]; then
-    FPATH="$HOMEBREW_COMPLETIONS:$FPATH"
+    FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+    autoload -Uz compinit
+    compinit
   fi
 fi
