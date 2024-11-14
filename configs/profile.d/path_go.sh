c0f82255a831b194e4abd1f2aa5e62f0154526b8
Bret Comnes <bcomnes@gmail.com>
2024-11-14 14:52:42 -0800

    Move git into .config

--- configs/profile.d/path_go.sh
+++ configs/profile.d/path_go.sh
@@ -1,6 +1,9 @@
 # Enable go path
 # https://github.com/bcomnes/goref#configure
 
+# Turn off GOTELEMETRY
+export GOTELEMETRY=off
+
 if [ -d $HOME/go ];
   then export GOPATH=$HOME/go ;
   export PATH=$PATH:$GOPATH/bin ;
