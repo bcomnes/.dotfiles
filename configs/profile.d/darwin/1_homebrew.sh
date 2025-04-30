e710b8349c6a6ea71ef0900c7df2d9aa3b584559
Bret Comnes <bcomnes@gmail.com>
2025-04-30 11:09:34 -0700

    Disable home brew analytics

--- configs/profile.d/darwin/1_homebrew.sh
+++ configs/profile.d/darwin/1_homebrew.sh
@@ -1,3 +1,4 @@
+export HOMEBREW_NO_ANALYTICS=1
 if [ "$(command -v /opt/homebrew/bin/brew)" ]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
   if [[ -d /opt/homebrew ]] && [ ! "$(command -v brew)" ]; then
